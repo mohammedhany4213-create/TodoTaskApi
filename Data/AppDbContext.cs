551dd5e24eef3c22d74f428aea70ed6aa19a86d4
mohammedhany4213-create <mohammedhany4213@gmail.com>
2026-09-05 00:25:01 +0300

    security: enforce unique user emails and explicit task relationship

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,17 +1,30 @@
-using Microsoft.EntityFrameworkCore ;
-using TodoApi.Models ;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
 
+namespace TodoApi.Data;
 
-namespace TodoApi.Data
+public class AppDbContext : DbContext
 {
-    public class AppDbContext : DbContext
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options)
-        :base (options)
-        {
-            
-        }
-        public DbSet<TodoTask> Tasks{get; set;}
-        public DbSet <User> Users {get; set;}
+    }
+
+    public DbSet<TodoTask> Tasks => Set<TodoTask>();
+    public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Email)
+            .IsUnique();
+
+        modelBuilder.Entity<User>()
+            .HasMany(u => u.Tasks)
+            .WithOne(t => t.User)
+            .HasForeignKey(t => t.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
