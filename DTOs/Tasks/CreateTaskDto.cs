f23b29fa427f02894d52b24f18378672752a467c
mohammedhany4213-create <mohammedhany4213@gmail.com>
2026-09-05 00:24:45 +0300

    refactor: remove server-controlled fields from create task DTO

--- DTOs/Tasks/CreateTaskDto.cs
+++ DTOs/Tasks/CreateTaskDto.cs
@@ -1,18 +1,16 @@
-using System.ComponentModel.DataAnnotations ;
+using System.ComponentModel.DataAnnotations;
+
 namespace TodoApi.DTOs.Tasks;
 
 public class CreateTaskDto
 {
     [Required]
     [MaxLength(100)]
-    public string Title {get; set;} = string.Empty ;
+    public string Title { get; set; } = string.Empty;
 
     [Required]
     [MaxLength(500)]
-    public string Description {get; set;} = string.Empty ;
+    public string Description { get; set; } = string.Empty;
 
-    public bool IsCompleted {get; set;} = false ;
-
-    public DateTime CreatedAt { get; set; }
-    public DateTime? DueDate {get; set;}
+    public DateTime? DueDate { get; set; }
 }
