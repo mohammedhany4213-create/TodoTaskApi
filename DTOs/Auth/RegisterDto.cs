a07b35a0a31c73a48cb381a330c19c058d1e37ec
mohammedhany4213-create <mohammedhany4213@gmail.com>
2026-09-05 00:24:36 +0300

    security: strengthen password validation

--- DTOs/Auth/RegisterDto.cs
+++ DTOs/Auth/RegisterDto.cs
@@ -1,18 +1,20 @@
-using System.ComponentModel.DataAnnotations ;
-namespace TodoApi.DTOs.Auth ;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoApi.DTOs.Auth;
 
 public class RegisterDto
 {
     [Required]
     [MaxLength(100)]
-    public string UserName {get; set;} = string.Empty ;
+    public string UserName { get; set; } = string.Empty;
 
     [Required]
     [EmailAddress]
     [MaxLength(100)]
-    public string Email {get;  set;} = string.Empty ;
+    public string Email { get; set; } = string.Empty;
 
     [Required]
-    [MaxLength(8)]
-    public string Password {get; set;} = string.Empty ;
+    [MinLength(8)]
+    [MaxLength(128)]
+    public string Password { get; set; } = string.Empty;
 }
