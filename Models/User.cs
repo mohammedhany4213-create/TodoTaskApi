6bfcf1ef267b63b7ef57b3bd29cbe3b8b5b6fcc4
mohammedhany4213-create <mohammedhany4213@gmail.com>
2026-09-05 17:12:51 +0300

    Clean up user model

--- Models/User.cs
+++ Models/User.cs
@@ -1,20 +1,22 @@
-using System.ComponentModel.DataAnnotations ;
-namespace TodoApi.Models ;
-public class User
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoApi.Models;
+
+public sealed class User
 {
-    public int Id {get; set;}
+    public int Id { get; set; }
 
     [Required]
     [MaxLength(100)]
-    public string UserName {get; set;} = string.Empty ;
+    public string UserName { get; set; } = string.Empty;
 
     [Required]
     [MaxLength(100)]
     [EmailAddress]
-    public string Email {get; set;} = string.Empty ;
+    public string Email { get; set; } = string.Empty;
 
     [Required]
-    public string PasswordHash {get; set;} = string.Empty ;
+    public string PasswordHash { get; set; } = string.Empty;
 
-    public ICollection<TodoTask> Tasks {get; set;} = new List<TodoTask>() ;
+    public ICollection<TodoTask> Tasks { get; set; } = new List<TodoTask>();
 }
