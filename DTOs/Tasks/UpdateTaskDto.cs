52ba5a130dff7440266b0bf7d7b147bd22177b72
mohammedhany4213-create <mohammedhany4213@gmail.com>
2026-09-05 17:10:08 +0300

    Clean up update task DTO

--- DTOs/Tasks/UpdateTaskDto.cs
+++ DTOs/Tasks/UpdateTaskDto.cs
@@ -2,14 +2,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace TodoApi.DTOs.Tasks;
 
-public class UpdateTaskDto
+public sealed class UpdateTaskDto
 {
     [Required]
     [MaxLength(100)]
     public string Title { get; set; } = string.Empty;
 
     [MaxLength(500)]
-    public string Description { get; set; } = string.Empty ;
+    public string Description { get; set; } = string.Empty;
 
     public bool IsCompleted { get; set; }
 
