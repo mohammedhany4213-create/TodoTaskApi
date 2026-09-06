9ae96d8861a9afe2201dfb31fe909d043c497540
mohammedhany4213-create <mohammedhany4213@gmail.com>
2026-09-06 22:16:31 +0300

    feat: add paginated task response

---
+++ server/DTOs/Tasks/PagedResultDto.cs
@@ -0,0 +1,12 @@
+namespace TodoApi.DTOs.Tasks;
+
+public sealed class PagedResultDto<T>
+{
+    public IReadOnlyList<T> Items { get; init; } = [];
+    public int PageNumber { get; init; }
+    public int PageSize { get; init; }
+    public int TotalCount { get; init; }
+    public int TotalPages => PageSize == 0 ? 0 : (int)Math.Ceiling(TotalCount / (double)PageSize);
+    public bool HasNextPage => PageNumber < TotalPages;
+    public bool HasPreviousPage => PageNumber > 1;
+}
